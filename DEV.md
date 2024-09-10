2b9320161d3583a7b7fb8eee843966552669af97
Ryu TANABE <tanabe@eyetech.jp>
2024-09-10 14:20:32 +0900

    add UI & Icon

---
+++ DEV.md
@@ -0,0 +1,14 @@
+# Development Memo
+
+```sh
+npx create-remix@latest
+cd remix-app-base
+bun run dev
+
+bun add @nextui-org/react framer-motion
+bun add remixicon
+```
+
+- [Remix](https://remix.run/)
+- [nextUI](https://nextui.org/)
+- [RemixIcon](https://remixicon.com/)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
 } satisfies Config;
